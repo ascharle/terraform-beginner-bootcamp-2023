b736d9a834aae7c036810819e882b13812601464
Charles Asirifi <asirificharles@yahoo.com>
2023-09-19 23:56:43 +0000

    modified main.tf and added aws s3 resources

--- main.tf
+++ main.tf
@@ -1,1 +1,33 @@
 
+terraform {
+  required_providers {
+
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+      
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+
+provider "aws" {
+ region = "eu-central-1"
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-4h5ty"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
